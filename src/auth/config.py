e68a3dc60377b0fc571cafefabae09ed40d46964
ilya-4real <ilya.chernishyov@gmail.com>
2024-03-16 15:19:24 +0200

    combined config files

--- src/auth/config.py
+++ src/auth/config.py
@@ -1,14 +1,3 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
 from passlib.context import CryptContext
 
-
-class AuthSettings(BaseSettings):
-    model_config = SettingsConfigDict(env_file="src/auth/.env_auth", extra="allow")
-
-    jwt_secret: str
-    jwt_algorithm: str
-    token_expiration_in_minutes: int
-
-
-auth_settings = AuthSettings()  # type: ignore
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
