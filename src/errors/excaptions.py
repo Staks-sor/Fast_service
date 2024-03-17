357a82cb8d9f90fb24f7e7211b61131bfdaeeaf3
NezBut <xyi12312354@mail.ru>
2024-03-18 01:31:52 +0700

    added error and handler for it

---
+++ src/errors/excaptions.py
@@ -0,0 +1,14 @@
+from fastapi import HTTPException
+
+
+class HTTPError(HTTPException):
+    def __init__(
+            self,
+            status_code: int,
+            *,
+            errors: list[str],
+            detail: str = None,
+            headers: dict[str, str] = None
+    ):
+        super().__init__(status_code=status_code, detail=detail, headers=headers),
+        self.errors = errors
