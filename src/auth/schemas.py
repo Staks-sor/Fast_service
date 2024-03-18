b1fb073d0a9866c695e08b8bd74c94eac5e41b43
NezBut <xyi12312354@mail.ru>
2024-03-18 17:03:40 +0700

    minor changes

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel, EmailStr, ConfigDict
 from datetime import datetime
 from uuid import UUID
 
+from pydantic import BaseModel, EmailStr, ConfigDict
+
 
 class UserCreateSchema(BaseModel):
-
     name: str
     email: EmailStr
     password: str
@@ -15,7 +15,7 @@ class UserToken(BaseModel):
     exp: datetime
 
 
-class UserResponce(BaseModel):
+class UserResponse(BaseModel):
     model_config = ConfigDict(from_attributes=True, extra="ignore")
     id: UUID
     name: str
