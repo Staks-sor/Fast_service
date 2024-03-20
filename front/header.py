c1efab775df8d0557a07c92261d17bdd3ac5f4f8
staks <stas.sor@gmail.com>
2024-03-20 06:26:16 +0300

    положил начало фронту, на флет попробуем как оно будет, в poetry не добавил если что pip instal flet

---
+++ front/header.py
@@ -0,0 +1,31 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "хз"
+
+    def route_change(route):
+        page.views.clear()
+        page.views.append(
+            ft.View(
+                "/",
+                [
+                    ft.AppBar(title=ft.Text("Автосервис"), bgcolor=ft.colors.SURFACE_VARIANT),
+
+                ],
+            )
+        )
+
+        page.update()
+
+    def view_pop(view):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view.route)
+
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
+    page.go(page.route)
+
+
+ft.app(target=main, view=ft.AppView.WEB_BROWSER)
