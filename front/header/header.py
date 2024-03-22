80d09fe1a9861b7d4a6c50f591d8a93593f95a12
staks <stas.sor@gmail.com>
2024-03-22 19:19:03 +0300

    добавил вход ну не связывал

--- front/header/header.py
+++ front/header/header.py
@@ -13,16 +13,30 @@ def main(page: ft.Page):
                                       ft.TextField(hint_text="email", autofocus=True, col=10, scale=0.9),
                                       ft.TextField(hint_text="Телефон", scale=0.9),
                                       ft.TextField(hint_text="Пароль", password=True, scale=0.9),
-                                      ft.TextField(hint_text="говно", scale=0.9),
+
                                       ft.CupertinoActionSheetAction(
                                           content=ft.Text("Зарегестрироваться")),
                                   ]
                                   )
+    dlg_enter_user = ft.AlertDialog(adaptive=True,
+                                    title=ft.Text("Вход", text_align="center"),
+                                    actions=[
+                                        ft.TextField(hint_text="email", autofocus=True, col=10, scale=0.9),
+                                        ft.TextField(hint_text="Пароль", password=True, scale=0.9),
+
+                                        ft.CupertinoActionSheetAction(
+                                            content=ft.Text("Вход")),
+                                    ]
+                                    )
 
     def open_reg(e):
         page.dialog = dlg_reg_user
         dlg_reg_user.open = True
+        page.update()
 
+    def open_enter_user(e):
+        page.dialog = dlg_enter_user
+        dlg_enter_user.open = True
         page.update()
 
     def route_change(route, login_function=None, registration_function=None, check_item_clicked=None):
@@ -37,7 +51,7 @@ def main(page: ft.Page):
                         actions=[
                             ft.IconButton(ft.icons.WB_SUNNY_OUTLINED),
                             ft.OutlinedButton(content=ft.Text("Регистрация"), on_click=open_reg, scale=0.9),
-                            ft.OutlinedButton(content=ft.Text("Вход"), scale=0.9)
+                            ft.OutlinedButton(content=ft.Text("Вход"), on_click=open_enter_user, scale=0.9)
                         ],
 
                     ),
