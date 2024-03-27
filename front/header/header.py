06c980ee2ac21dfd5e676d98f89cb5bcc72c61ad
staks <stas.sor@gmail.com>
2024-03-27 19:36:01 +0300

    сделал небольшие правки, добавил комментарии. добавил футер.

--- front/header/header.py
+++ front/header/header.py
@@ -1,13 +1,19 @@
 import json
 import time
 import flet as ft
+from flet import View, Page, AppBar, ElevatedButton, Text
+from flet import RouteChangeEvent, ViewPopEvent, CrossAxisAlignment, MainAxisAlignment
 import requests
+from flet_core import TextAlign
 
 
 def main(page: ft.Page):
+    # Устанавливаем заголовок страницы
     page.title = "Главная"
 
+    # Функция для создания модальных окон
     def modal_window():
+        # Окно для регистрации
         dlg_reg_user = ft.AlertDialog(
             adaptive=True,
             title=ft.Text("Регистрация", text_align="center"),
@@ -22,6 +28,7 @@ def main(page: ft.Page):
                 )
             ]
         )
+        # Окно для входа
         dlg_enter_user = ft.AlertDialog(
             adaptive=True,
             title=ft.Text("Вход", text_align="center"),
@@ -33,11 +40,13 @@ def main(page: ft.Page):
         )
         return dlg_reg_user, dlg_enter_user
 
+    # Функция для открытия диалогового окна
     def open_dialog(dialog):
         page.dialog = dialog
         dialog.open = True
         page.update()
 
+    # Функция для регистрации пользователя
     def registrator(e, dlg_reg_user):
         name = dlg_reg_user.actions[0].value
         email = dlg_reg_user.actions[1].value
@@ -51,6 +60,7 @@ def main(page: ft.Page):
         response = requests.post(url, json=data)
         response_json = response.json()
         if response.status_code == 200:
+            # Успешная регистрация
             dlg_success_registration = ft.AlertDialog(
                 adaptive=True,
                 title=ft.Text("Вы успешно зарегистрированы", text_align="center")
@@ -60,6 +70,7 @@ def main(page: ft.Page):
             dlg_success_registration.open = False
             page.update()
         else:
+            # Ошибка при регистрации
             error_description = response_json['detail'][0]['msg']
             dlg_error_registration = ft.AlertDialog(
                 adaptive=True,
@@ -70,23 +81,37 @@ def main(page: ft.Page):
             open_dialog(dlg_reg_user)
             print("Ошибка при регистрации пользователя:", response.text)
 
-    def route_change(route, login_function=None, registration_function=None, check_item_clicked=None):
+    # Функция для изменения маршрута
+    def route_change(e: RouteChangeEvent) -> None:
+        # Создаем модальные окна
         dlg_reg_user, dlg_enter_user = modal_window()
+        # Очищаем предыдущие представления
         page.views.clear()
+        # Добавляем новое представление
         page.views.append(
             ft.View(
-                "/",
-                [
+                route="/",
+                controls=[
+                    # Футер
+                    ft.BottomAppBar(
+                        bgcolor=ft.colors.SURFACE_VARIANT,
+                        shape=ft.NotchShape.CIRCULAR,
+                        height=45,
+                        content=Text("this is footer", size=10, text_align=TextAlign.CENTER)
+                    ),
+                    # Верхняя панель приложения
                     ft.AppBar(
                         title=ft.Text("Автосервис"),
                         bgcolor=ft.colors.SURFACE_VARIANT,
                         actions=[
                             ft.IconButton(ft.icons.WB_SUNNY_OUTLINED),
+                            # Кнопка "Регистрация"
                             ft.OutlinedButton(
                                 content=ft.Text("Регистрация"),
                                 on_click=lambda e: open_dialog(dlg_reg_user),
                                 scale=0.9
                             ),
+                            # Кнопка "Вход"
                             ft.OutlinedButton(
                                 content=ft.Text("Вход"),
                                 on_click=lambda e: open_dialog(dlg_enter_user),
@@ -99,14 +124,17 @@ def main(page: ft.Page):
         )
         page.update()
 
+    # Функция для удаления представления из стека
     def view_pop(view):
         page.views.pop()
         top_view = page.views[-1]
         page.go(top_view.route)
 
+    # Привязываем функции к событиям
     page.on_route_change = route_change
     page.on_view_pop = view_pop
     page.go(page.route)
 
 
-ft.app(target=main, view=ft.AppView.WEB_BROWSER)
+if __name__ == "__main__":
+    ft.app(target=main, view=ft.AppView.WEB_BROWSER)
